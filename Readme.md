47282bcb0de65f83f083328aafe653801ba6dc10
Asim Fida <asimfida1981@gmail.com>
2024-07-19 19:01:31 +0500

    add models and metheds in models

--- Readme.md
+++ Readme.md
@@ -26,3 +26,57 @@ app.use(express.static("public")); -->
 <!-- app.use(cookieParser()); -->
 
 # setup utils folder for ApiErrors, ApiResponse,asynHandler..need deepstudy
+
+# develop models (user and video)
+# npm i mongoose-aggregate-paginate-v2 for models aggregation
+# import mongooseaggregationpagination in video model and use it.
+# Like videoSchema.plugin(mongooseAggregatePaginate);
+# install bcrypt and jsonwebtoken and import in usermodal . we can use it with middlewares like pre
+# read mongoose middlewares 
+# Write below code for encpyt password and compare password in usermodel
+<!-- userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}; -->
+# work on jwt update .env
+<!-- ACCESS_TOKEN_SECRET=chaiaurcode
+ACCESS_TOKEN_EXPIRY = 1d
+
+REFRESH_TOKEN_SECRET=chaiaurcode2
+REFRESH_TOKEN_EXPIRY=10d -->
+# type in userschema.methed
+<!-- userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.getRefreshSignedJwtToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+}; -->
+#
+#
---
+++ src/models/User.model.js
@@ -0,0 +1,91 @@
+import mongoose, { models } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+  {
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: { unique: true },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      index: { unique: true },
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.getRefreshSignedJwtToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+export default User = models.User || mongoose.model("User", userSchema);
